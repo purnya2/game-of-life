4d8b9576ae48114add42e8dc5988a6c1ea87d6d6
purnya2 <purnya.mail@gmail.com>
2025-08-01 17:32:57 +0200

    now the cells know if they die or survive

--- cell.gd
+++ cell.gd
@@ -2,5 +2,45 @@ class_name Cell
 
 extends Node3D
 
+signal calculation_finished
+
 var gridposition : Vector3i
-var state : bool = false
+var alive : bool = true
+var instance_map : Dictionary[Vector3i, Cell]
+
+var alive_radius : int = 1
+var neighbors_count_needed_to_stay_alive = 2
+
+var neighbors : Array = []
+var calculated_if_alive : bool = false
+
+#var empty_neightbors : Dictio
+## calcualate if you can be alive
+## give neighboring cells a value
+
+func _process(delta) -> void:
+	if !calculated_if_alive:
+		check_if_alive()
+
+func check_if_alive() -> void:
+	var neighbor_count : int = 0
+	
+	for x in range(gridposition.x - alive_radius, gridposition.x + alive_radius+1):
+			for y in range(gridposition.y - alive_radius, gridposition.y + alive_radius+1):
+					for z in range(gridposition.z - alive_radius, gridposition.z + alive_radius+1):
+						var neighbor_pos = Vector3i(x,y,z)
+						if neighbor_pos != gridposition :
+							if instance_map.has(neighbor_pos):
+								neighbor_count += 1
+	if neighbor_count >= neighbors_count_needed_to_stay_alive: #LOL??
+		alive = true
+	else :
+		$CSGBox3D.material_override = StandardMaterial3D.new()
+		$CSGBox3D.material_override.albedo_color = Color.RED
+		alive = false
+	calculated_if_alive = true
+	emit_signal("calculation_finished")
+
+func reset()-> void:
+	calculated_if_alive = false
+	neighbors = []
--- game_grid.gd
+++ game_grid.gd
@@ -2,9 +2,9 @@ extends Node3D
 
 var cell_scene = preload("res://cell.tscn")
 
-var gridspacing_x : float = 2.0
-var gridspacing_y : float = 2.0
-var gridspacing_z : float = 2.0
+var gridspacing_x : float = 1.2
+var gridspacing_y : float = 1.2
+var gridspacing_z : float = 1.2
 
 var x_size = 100
 var y_size = 100
@@ -15,18 +15,28 @@ var cell_instances = []
 
 var t : int = 0
 var time_accumulated : float = 0
-var time_interval : float = 0.5		# with this you can decide how fast or slow the evolution is
+var time_interval : float = 0.1		# with this you can decide how fast or slow the evolution is
 
 
 
 var instance_map : Dictionary[Vector3i, Cell]
-var children_map: Dictionary[Vector3i,Cell]
+var children_map: Dictionary[Vector3i,Cell] # I need this because I have to know which children are still represented graphically and which aren't
+var neighbor_map : Dictionary[Vector3i, int]
+
 var commands : Dictionary[String, Array]
 
 
+
 func _ready() -> void:
+	var alive = []
+	for x in range(-10,15):
+		alive.append(Vector3i(x,0,0))
+	for z in range(-10,10):
+		alive.append(Vector3i(0,0,z))
+	for y in range(-10,10):
+		alive.append(Vector3i(0,y,0))
 	commands = {
-	"alive" : [Vector3i(0,0,0),Vector3i(1,0,0),Vector3i(1,1,1),Vector3i(-1,-1,-1),Vector3i(0,0,-1)],
+	"alive" : alive,
 	"dead" : []
 	}
 	
@@ -38,14 +48,16 @@ func _process(delta: float) -> void:
 	if time_accumulated >= time_interval:
 		time_accumulated = 0.0
 		
-		#print("t = " + str(t))
-
+		print("t = " + str(t))
 
 		execute_commands()		# Execute commands
-		#reposition_cells()			# Reposition cells from Logical to Effective position
-		#var next_alives : Array = calculate_alive()			# Calculate which cells will stay alive
+		var next_deads : Array = await calculate_dead()			# Calculate which cells will stay alive, interrogate here each cell
 		#var next_deads : Array = calculate_dead()				# Calculate which ones will be dead
-		#construct_commands(next_alives, next_deads)		# Make commands for the next time loop
+		
+		var next_alives = []
+		commands = construct_commands(next_alives, next_deads)		# Make commands for the next time loop, 
+		
+		reset_cells()
 		t += 1						# Progress timestamp
 
 
@@ -54,21 +66,45 @@ func _physics_process(delta) -> void:
 	var gridspacing = Vector3(gridspacing_x+ %XSpacingSlider.value, gridspacing_y+%YSpacingSlider.value,gridspacing_z+%ZSpacingSlider.value)
 	reposition_cells(gridspacing)
 
+func calculate_dead():
+	## for now this only calculates who STAYS dead, and not who is born...!!
+	var out : Array[Vector3i] = []
+	for key_pos in instance_map:
+			
+		var cell = instance_map[key_pos]
+		
+		if(cell.calculated_if_alive != true):
+			await cell.calculation_finished
+		if !cell.alive:
+			out.append(key_pos)
+	
+	return out
 
 func execute_commands() -> void :
 	for alive_cell_pos in commands["alive"] :
 		if !instance_map.has(alive_cell_pos):
-			print("executing alive")
-			var cell_instance = cell_scene.instantiate()
-			#cell_instance.gridposition = alive_cell_pos
+			var cell_instance : Cell = cell_scene.instantiate()
+			# populate the cell instance's properties
+			cell_instance.gridposition = alive_cell_pos
+			cell_instance.instance_map = instance_map
 			instance_map[alive_cell_pos] = cell_instance
 			
 	for dead_cell_pos in commands["dead"] :
 		instance_map[dead_cell_pos].queue_free()
 		instance_map.erase(dead_cell_pos)
 		
-func reposition_cells(gridspacing : Vector3) -> void : 
+		
+func construct_commands(next_alives, next_deads) -> Dictionary[String, Array] : 
+	var new_commands : Dictionary[String, Array]
+	new_commands = {
+		"alive" : next_alives,
+		"dead" : next_deads
+	}
 	
+	return new_commands
+
+
+func reposition_cells(gridspacing : Vector3) -> void : 
 	var scale_matrix = Transform3D().scaled(gridspacing)
 	
 	for key_pos in instance_map:
@@ -78,17 +114,9 @@ func reposition_cells(gridspacing : Vector3) -> void :
 			cell_instance.position = scale_matrix*Vector3(key_pos)
 			add_child(cell_instance)
 			children_map[key_pos] = cell_instance
-			print("adding!")
 		else:
 			children_map[key_pos].position = scale_matrix*Vector3(key_pos)
 
-		
-
-func birth(pos : Vector3):
-	var cell_instance = cell_scene.instantiate()
-
-	cell_instance.position = pos
-	add_child(cell_instance)
-	
-func check_cell_status(pos):
-	return 
+func reset_cells() -> void :
+	for key_pos in instance_map:
+		instance_map[key_pos].reset()
